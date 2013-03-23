e331dc397cc51a094210aa19d2b419a5fad954f6
Bill Tozier <bill@vagueinnovation.com>
2013-03-23 12:47:30 -0400

    more fiddling with hillclimbing spike

--- spikes/hillclimb_spike.rb
+++ spikes/hillclimb_spike.rb
@@ -13,7 +13,7 @@ end
 
 def random_script(length)
   words = length.times.collect {@tokens.sample}
-  words = words.collect {|w| w=="k" ? (clean_float(10).to_s) : w}
+  words = words.collect {|w| w=="k" ? (clean_float(100).to_s) : w}
   return words.join(" ")
 end
 
@@ -21,25 +21,30 @@ end
 def mutant_from_script(script,probability_per_token=0.1)
   wt_tokens = script.split
   mutant_tokens = wt_tokens.collect {|t| Random.rand()<=probability_per_token ? @tokens.sample : t}
-  mutant_tokens = mutant_tokens.collect {|w| (w=="k") ? (clean_float(10).to_s) : w}
+  mutant_tokens = mutant_tokens.collect {|w| (w=="k") ? (clean_float(100).to_s) : w}
   return Snip::Answer.new(mutant_tokens.join(" "))
 end
 
 
-3.times do |run_number|
+5.times do |run_number|
   File.open("./hillclimb_spike-#{run_number}.out", "w") do |output_file|
-    climb_length = 500
+    climb_length = 250
     mutation_rate = 0.5
-    output_file.puts "t,mutation_rate,current_score,current_script,current_wtf,mutant_score,mutant_script,mutant_wtf"
+    change_records = []
+    improvement_records = []
+    silent_records = []
+    moving_average_window = 100
+
+    output_file.puts "t,mutation_rate,change_rate,silent_rate,improvement_rate,current_score,current_script,current_wtf,mutant_score,mutant_script,mutant_wtf"
 
     answer = Snip::Answer.new(random_script(40))
     evaluator.evaluate(answer)
     answer_score = [answer.scores[:forward_balanced_accuracy],answer.scores[:backward_balanced_accuracy]].max
     answer_wtf = Snip::Interpreter.new(script:answer.script).wtf.inspect
     
-    output_file.puts "0,#{mutation_rate},#{answer_score},#{answer.script.inspect},#{answer_wtf},,"
+    output_file.puts "0,#{mutation_rate},1.0,1.0,1.0,#{answer_score},#{answer.script.inspect},#{answer_wtf},,"
 
-    500.times do |start|
+    400.times do |start|
       p "mutation rate: #{mutation_rate.round(5)}, best score: #{answer_score.round(5)}, best model: #{answer_wtf.delete('"')}"
       climb_length.times do |step|
         mutant = mutant_from_script(answer.script,mutation_rate)
@@ -50,8 +55,27 @@ end
         mutant_score = [mutant.scores[:forward_balanced_accuracy],mutant.scores[:backward_balanced_accuracy]].max
         mutant_wtf = Snip::Interpreter.new(script:mutant.script).wtf.inspect
 
-        answer = mutant if answer_score <= mutant_score
-        output_file.puts "#{climb_length*start + step},#{mutation_rate},#{answer_score},#{answer.script.inspect},#{answer_wtf},#{mutant_score},#{mutant.script.inspect},#{mutant_wtf}"
+        if answer_score <= mutant_score
+          change_records << (answer_wtf != mutant_wtf)
+          silent_records << ((answer_wtf == mutant_wtf) && (answer.script != mutant.script))
+          improvement_records << (answer_score < mutant_score)
+          answer = mutant
+        else
+          change_records << false
+          silent_records << false
+          improvement_records << false
+        end
+
+        change_records = change_records[-moving_average_window..-1] if change_records.length > moving_average_window
+        silent_records = silent_records[-moving_average_window..-1] if silent_records.length > moving_average_window
+        improvement_records = improvement_records[-moving_average_window..-1] if improvement_records.length > moving_average_window
+
+        change_ma = change_records.count(true).to_f/change_records.length
+        silent_ma = silent_records.count(true).to_f/silent_records.length
+        improvement_ma = improvement_records.count(true).to_f/improvement_records.length
+        # p [change_ma,improvement_ma]
+
+        output_file.puts "#{climb_length*start + step},#{mutation_rate},#{change_ma.round(5)},#{silent_ma.round(5)},#{improvement_ma.round(5)},#{answer_score},#{answer.script.inspect},#{answer_wtf},#{mutant_score},#{mutant.script.inspect},#{mutant_wtf}"
       end
       mutation_rate = (mutation_rate < 0.01 ? 0.5 : (mutation_rate * 0.5))
     end
--- spikes/hillclimb_spike_plot.r
+++ spikes/hillclimb_spike_plot.r
@@ -1,6 +1,8 @@
 # R script for plotting a simple trace of sampled Snip scripts, mutation rate, and best score so far from data
 
-climb = read.csv("~/programming/dartmouth/ces_like/spikes/hillclimb_spike-0.out", header=TRUE)
-plot(climb$mutant_score,ylim=c(0.0,1.0),cex=0.3,pch=4,col=rgb(0.2,0.3,0.6,0.5),main="periodic annealing of Snip scripts",xlab="variant",ylab="Balanced Accuracy score")
-points(climb$current_score,cex=0.3,pch=3,col=rgb(0.9,0.05,0.1,0.01))
-points(climb$mutation_rate,cex=0.3,pch=3,col=rgb(0.1,0.6,0.1,0.01))
+climb = read.csv("~/programming/dartmouth/ces_like/spikes/hillclimb_spike-0.out", header=TRUE); 
+  plot(climb$mutant_score,ylim=c(0.0,1.0),cex=0.3,pch=4,col=rgb(0.2,0.3,0.6,0.5),main="multiscale hillclimbing of Snip scripts",xlab="variant",ylab="Balanced Accuracy score"); 
+  points(x=climb$improvement_rate,cex=0.3,pch=2,col=rgb(0.9,0.1,0.1,0.3),type="l"); 
+  points(climb$silent_rate,cex=0.3,pch=3,col=rgb(0.1,0.9,0.1,0.3),type="l"); 
+  points(climb$change_rate,cex=0.3,pch=3,col=rgb(0.1,0.1,0.9,0.3),type="l");
+  legend(100,1.0,c("improved","silent","expressed"),col=c(rgb(0.9,0.1,0.1,0.3),rgb(0.1,0.9,0.1,0.3),rgb(0.1,0.1,0.9,0.3)),lty=1)
