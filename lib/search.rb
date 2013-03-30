21a32aaf14895f83382b2bdb4a613dec56be1559
Bill Tozier <bill@vagueinnovation.com>
2013-03-30 09:02:12 -0400

    moved Evaluators to Search module

--- lib/search.rb
+++ lib/search.rb
@@ -44,4 +44,119 @@ module Search
     end
 
   end
+
+
+  class Evaluator
+    def evaluate(answer)
+      answer.scores[:generic] = nil
+    end
+
+    
+    def self.median(numbers)
+      raise ArgumentError.new("no nil values in median calculation") if numbers.include?(nil)
+      raise ArgumentError.new("median cannot be calculated for an empty list") if numbers.compact.empty?
+      sorted = numbers.compact.sort
+      count = sorted.length
+      result = case 
+      when sorted.empty?
+        nil
+      when count.even?
+        (sorted[count/2 - 1].to_f + sorted[count/2]) / 2.0
+      else
+        sorted[count/2].to_f
+      end
+      return result
+    end
+  end
+
+
+  class EvaluatorError < StandardError
+    def initialize(msg = "data file missing 'group' column")
+      super(msg)
+    end
+  end
+  
+  
+  class BalancedAccuracyEvaluator < Evaluator
+    attr_reader :data
+    attr_reader :positive_examples
+    attr_reader :negative_examples
+    
+    
+    def initialize(data)
+      data.each do |r|
+        raise ArgumentError.new("A record has no :group assigned") if r[:group].nil?
+        raise ArgumentError.new("A record as a :group assigned that is not 0 or 1") unless 
+          [0,1].include? r[:group]
+      end
+      @data = data
+      @positive_examples, @negative_examples = @data.partition {|r| r[:group] == 1}
+    end
+
+    def self.from_data_file(path)
+      require 'csv'
+      rows = []
+      CSV.foreach(path, headers:true, converters: :numeric) do |file_row|
+        row = file_row.to_hash
+        row[:group] = row["group"]
+        row.delete("group")
+        rows << row
+      end
+      return BalancedAccuracyEvaluator.new(rows)
+    end
+    
+    
+    def run_examples(answer,rows)
+      rows.collect do |row|
+        Snip::Interpreter.new(script:answer.script, bindings:row).run.emit!
+      end
+    end
+    
+    
+    def apply_threshold_to_predictions(numbers,threshold)
+      numbers.collect {|num| num <= threshold ? 0 : 1 }
+    end
+    
+    
+    def balanced_accuracy(predicted_positives, predicted_negatives)
+      false_positives = predicted_negatives.count(1).to_f
+      false_negatives = predicted_positives.count(0).to_f
+      true_positives = predicted_positives.count(1).to_f
+      true_negatives = predicted_negatives.count(0).to_f
+      
+      sensitivity = true_positives / (true_positives + false_negatives)
+      specificity = true_negatives / (true_negatives + false_positives)
+      
+      return (sensitivity + specificity) / 2
+    end
+
+
+    def evaluate(answer)
+      pos_values = run_examples(answer,@positive_examples)
+      neg_values = run_examples(answer,@negative_examples)
+
+      all_values = pos_values + neg_values
+      if all_values.include?(nil) || pos_values.empty? || neg_values.empty?
+        answer.scores[:balanced_accuracy_cutoff] = nil
+        answer.scores[:forward_balanced_accuracy] = nil
+        answer.scores[:backward_balanced_accuracy] = nil
+      else
+        pos_median = Search::Evaluator.median(pos_values)
+        neg_median = Search::Evaluator.median(neg_values)
+        threshold = (neg_median + pos_median) / 2
+
+        predicted_positives = apply_threshold_to_predictions(pos_values, threshold)
+        predicted_negatives = apply_threshold_to_predictions(neg_values, threshold)
+
+        answer.scores[:balanced_accuracy_cutoff] = threshold
+
+        forward_threshold_score = balanced_accuracy(predicted_positives, predicted_negatives)
+        backward_threshold_score = 1.0 - forward_threshold_score
+
+        answer.scores[:forward_balanced_accuracy] = forward_threshold_score
+        answer.scores[:backward_balanced_accuracy] = backward_threshold_score
+      end
+      return answer
+    end
+  end
 end
--- spec/evaluators_spec.rb
+++ spec/evaluators_spec.rb
@@ -4,12 +4,12 @@ describe "BalancedAccuracyEvaluator" do
   describe "initialization" do
     it "should accept a dataset as an argument" do
       one_datum = [{"x1" => 2, group:1}]
-      Snip::BalancedAccuracyEvaluator.new(one_datum).data.should == one_datum
+      Search::BalancedAccuracyEvaluator.new(one_datum).data.should == one_datum
     end
 
     it "should split the dataset into positive and negative example subsets" do
       simple_data = [{x1:2, group:0}, {x1:1, group:1}]
-      simpleEvaluator = Snip::BalancedAccuracyEvaluator.new(simple_data)
+      simpleEvaluator = Search::BalancedAccuracyEvaluator.new(simple_data)
       simpleEvaluator.positive_examples.length.should == 1
       simpleEvaluator.negative_examples.length.should == 1
       simpleEvaluator.data.length.should == 2
@@ -17,31 +17,31 @@ describe "BalancedAccuracyEvaluator" do
 
     it "should raise an exception if any of the records don't have a :group assigned" do
       bad_data = [{x1:2, group:0}, {x1:1, group:nil}]
-      lambda{ Snip::BalancedAccuracyEvaluator.new(bad_data) }.should raise_error
+      lambda{ Search::BalancedAccuracyEvaluator.new(bad_data) }.should raise_error
     end
 
     it "should raise an exception if any of the records don't have a :group key" do
       bad_data = [{x1:2, group:0}, {x1:1, :class => 1}]
-      lambda{ Snip::BalancedAccuracyEvaluator.new(bad_data) }.should raise_error
+      lambda{ Search::BalancedAccuracyEvaluator.new(bad_data) }.should raise_error
       other_bad_data = [{x1:2, group:0}, {x1:1, "group" => 1}]
-      lambda{ Snip::BalancedAccuracyEvaluator.new(other_bad_data) }.should raise_error
+      lambda{ Search::BalancedAccuracyEvaluator.new(other_bad_data) }.should raise_error
     end
 
     it "should raise an exception if any of the records' :group is not in [0,1]" do
       bad_data = [{x1:2, group:0}, {x1:1, group:2}]
-      lambda{ Snip::BalancedAccuracyEvaluator.new(bad_data) }.should raise_error
+      lambda{ Search::BalancedAccuracyEvaluator.new(bad_data) }.should raise_error
     end
   end
 
 
   describe "BalancedAccuracyEvaluator.from_data_file" do
     it "should take a file path" do
-      lambda{ Snip::BalancedAccuracyEvaluator.from_data_file }.should raise_error(ArgumentError)
+      lambda{ Search::BalancedAccuracyEvaluator.from_data_file }.should raise_error(ArgumentError)
     end
 
     it "should raise an exception if no column is named 'group'" do
       path = './spec/fixtures/no_group_column.csv'
-      lambda{ Snip::BalancedAccuracyEvaluator.from_data_file(path) }.should raise_error(ArgumentError)
+      lambda{ Search::BalancedAccuracyEvaluator.from_data_file(path) }.should raise_error(ArgumentError)
     end
 
     it "should raise an exception if any group measurement is missing"
@@ -50,13 +50,13 @@ describe "BalancedAccuracyEvaluator" do
     
     it "should read the header row to build the variable names list" do
       path = './spec/fixtures/five_columns.csv'
-      evaluator = Snip::BalancedAccuracyEvaluator.from_data_file(path)
+      evaluator = Search::BalancedAccuracyEvaluator.from_data_file(path)
       evaluator.data[-1].keys.should == ["X1", "X2", "X3", "X4", "X5", :group]
     end
 
     it "should create a data hash for every row of the file" do
       path = './spec/fixtures/five_columns.csv'
-      evaluator = Snip::BalancedAccuracyEvaluator.from_data_file(path)
+      evaluator = Search::BalancedAccuracyEvaluator.from_data_file(path)
       evaluator.data.length.should == 4
     end
   end
@@ -69,20 +69,20 @@ describe "BalancedAccuracyEvaluator" do
     end
 
     it "should write two scores to the answer's scores hash" do
-      Snip::BalancedAccuracyEvaluator.new(@simple_data).evaluate(@trivial_answer)
+      Search::BalancedAccuracyEvaluator.new(@simple_data).evaluate(@trivial_answer)
       @trivial_answer.scores[:forward_balanced_accuracy].should_not be_nil
       @trivial_answer.scores[:backward_balanced_accuracy].should_not be_nil
     end
 
     it "should run the script once for every row of data" do
-      scorer = Snip::BalancedAccuracyEvaluator.new(@simple_data)
+      scorer = Search::BalancedAccuracyEvaluator.new(@simple_data)
       runner = Snip::Interpreter.new(script:@trivial_answer.script)
       Snip::Interpreter.should_receive(:new).exactly(3).times.and_return(runner)
       scorer.evaluate(@trivial_answer)
     end
 
     it "should collect the top item of the stack every time the script is run" do
-      scorer = Snip::BalancedAccuracyEvaluator.new(@simple_data)
+      scorer = Search::BalancedAccuracyEvaluator.new(@simple_data)
       runner = Snip::Interpreter.new(script:@trivial_answer.script)
       Snip::Interpreter.stub!(:new).and_return(runner)
       runner.should_receive(:emit!).exactly(3).times
@@ -90,26 +90,26 @@ describe "BalancedAccuracyEvaluator" do
     end
 
     it "should calculate the median value for each class" do
-      scorer = Snip::BalancedAccuracyEvaluator.new(@simple_data)
-      Snip::Evaluator.should_receive(:median).exactly(2).times.and_return(77)
+      scorer = Search::BalancedAccuracyEvaluator.new(@simple_data)
+      Search::Evaluator.should_receive(:median).exactly(2).times.and_return(77)
       scorer.evaluate(@trivial_answer)
     end
 
     it "should save a :balanced_accuracy_threshold score in the answer" do
-      scorer = Snip::BalancedAccuracyEvaluator.new(@simple_data)
+      scorer = Search::BalancedAccuracyEvaluator.new(@simple_data)
       scorer.evaluate(@trivial_answer)
       @trivial_answer.scores[:balanced_accuracy_cutoff].should == 0.75
     end
 
     it "should return the balanced accuracy as ((tp / (tp + fn)) + (tn / (tn + fp))) / 2" do
-      scorer = Snip::BalancedAccuracyEvaluator.new(@simple_data)
+      scorer = Search::BalancedAccuracyEvaluator.new(@simple_data)
       scorer.evaluate(@trivial_answer)
       @trivial_answer.scores[:forward_balanced_accuracy].should == 1.0
       @trivial_answer.scores[:backward_balanced_accuracy].should == 0.0
 
 
       backwards = [{"x1" => 1, group:0}, {"x1" => 0, group:1}, {"x1" => 0, group:1}]
-      Snip::BalancedAccuracyEvaluator.new(backwards).evaluate(@trivial_answer)
+      Search::BalancedAccuracyEvaluator.new(backwards).evaluate(@trivial_answer)
       @trivial_answer.scores[:forward_balanced_accuracy].should == 0.0
       @trivial_answer.scores[:backward_balanced_accuracy].should == 1.0
     end
@@ -118,26 +118,26 @@ describe "BalancedAccuracyEvaluator" do
 
   describe "Evaluator.median(values)" do
     it "should raise an exception if there are any nil values in the list" do
-      lambda{ Snip::Evaluator.median([2,3,5,6,88,nil]) }.should raise_error
+      lambda{ Search::Evaluator.median([2,3,5,6,88,nil]) }.should raise_error
     end
 
     it "should raise an exception if the list is empty" do
-      lambda{ Snip::Evaluator.median([]) }.should raise_error
+      lambda{ Search::Evaluator.median([]) }.should raise_error
     end
 
     it "should return the middle value (after sorting) of an odd-length list" do
-      Snip::Evaluator.median([2,3,5,6,88]).should == 5.0
-      Snip::Evaluator.median([1,1,1]).should == 1.0
+      Search::Evaluator.median([2,3,5,6,88]).should == 5.0
+      Search::Evaluator.median([1,1,1]).should == 1.0
     end
 
     it "should return the average of the two middle numbers of an even-length list" do
-      Snip::Evaluator.median([-11,22,44,555]).should == 33.0
-      Snip::Evaluator.median([17,33]).should == 25.0
-      Snip::Evaluator.median([17,17]).should == 17.0
+      Search::Evaluator.median([-11,22,44,555]).should == 33.0
+      Search::Evaluator.median([17,33]).should == 25.0
+      Search::Evaluator.median([17,17]).should == 17.0
     end
 
     it "should return the only value for a 1-element list" do
-      Snip::Evaluator.median([13]).should == 13.0
+      Search::Evaluator.median([13]).should == 13.0
     end
   end
 end
